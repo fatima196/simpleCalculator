72d0dd827472fb94262639457cb524ec86a6abaa
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 17:40:54 +0200

    Update MyCalc_Form.java

--- MyCalc_Form.java
+++ MyCalc_Form.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mycalc;
 
-/**
- *
- * @author lenovo
- */
+
 public class MyCalc_Form extends javax.swing.JFrame {
 
     /**
