abab66d997935ef27a53331d1df606f07d45add5
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 17:23:49 +0200

    Add files via upload

---
+++ MyCalc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycalc;
+
+/**
+ *
+ * @author lenovo
+ */
+public class MyCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MyCalc_Form f =new MyCalc_Form();
+        f.setVisible(true);
+    }
+    
+}
